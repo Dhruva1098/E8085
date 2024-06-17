2029963fe01e0242fc4f9a7562d2452db3e4c039
Dhruva Sharma <57439920+Dhruva1098@users.noreply.github.com>
2024-06-17 19:58:14 +0530

    Update EMU8085.cpp

--- EMU8085.cpp
+++ EMU8085.cpp
@@ -1,5 +1,6 @@
+#include "flag.h"
+
 #include <iostream>
-#include "flag.cpp"
 
 uint8_t MEMORY[0xFF]; //__init__ memory 256x8 bytes
 uint8_t GPR[6]; //general purpose registers
@@ -8,8 +9,4 @@ uint16_t program_counter;
 uint16_t stack_pointer;
 
 int main() {
-	GPR[B] = 1;
-	GPR[C] = 0;
-	uint16_t* ptr = (uint16_t *) & GPR[B];
-	std::cout << unsigned(*ptr);
 }
