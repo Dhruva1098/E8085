ceab7a9dc835db3b87f582ae752234d7af376e2d
Dhruva Sharma <dhruva1098@gmail.com>
2024-06-21 12:39:38 +0530

    test

--- instruction_set.h
+++ instruction_set.h
@@ -6,22 +6,13 @@ extern uint8_t SPR[3]; //special purpose registers
 extern uint16_t program_counter;
 extern uint16_t stack_pointer;
 
-enum {
-    MOV = 0,
-    MVI,
-    LDA,
-    LDAX,
-    STA,
-    XCHG
-}
-
 void MOV(uint8_t R, uint16_t M);
 void MOV(uint16_t M, uint8_t data);
 void MVI(uint8_t R, uint8_t data);
 void LDA(uint16_t M);
 void LDAX(uint8_t R);
 void STA(uint16_t M);
-void XCHG(uint8_t R, uint8_t R);
+void XCHG(uint8_t R1, uint8_t R2);
 void SPHL(); //HL -> SP
 void PCHL(); //PC -> HL
 void PUSH(uint8_t R);
