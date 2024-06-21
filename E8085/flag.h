d27c56c347e1002072136b5af637234966766065
Dhruva Sharma <57439920+Dhruva1098@users.noreply.github.com>
2024-06-21 15:37:14 +0530

    Create flag.h

---
+++ E8085/flag.h
@@ -0,0 +1,84 @@
+//
+//  flag.h
+//  E8085
+//
+//  Created by Dhruva Sharma on 21/06/24.
+//
+
+#ifndef flag_h
+#define flag_h
+
+#include <iostream>
+
+class _FLAG_ {
+    uint8_t FLAG;
+public:
+    _FLAG_() :FLAG(0) {};
+    
+    bool get_sign() {
+        return unsigned(FLAG << 7);
+    }
+    void set_flag() {
+        FLAG |= 0b10000000;
+    }
+    void reset_flag() {
+        FLAG &= 0b01111111;
+    }
+
+    bool get_zero() {
+        return unsigned(FLAG << 6);
+    }
+    void set_zero() {
+        FLAG |= 0b01000000;
+    }
+    void reset_zero() {
+        FLAG &= 0b10111111;
+    }
+
+    bool get_aux_carry() {
+        return unsigned(FLAG << 4);
+    }
+    void set_aux_carry() {
+        FLAG |= 0b00001000;
+    }
+    void reset_aux_carry() {
+        FLAG &= 0b11110111;
+    }
+
+    bool get_parity() {
+        return unsigned(FLAG << 2);
+    }
+    void set_parity() {
+        FLAG |= 0b00000010;
+    }
+    void reset_parity() {
+        FLAG &= 0b11111101;
+    }
+
+    bool get_carry() {
+        return unsigned(FLAG << 1);
+    }
+    void set_carry() {
+        FLAG |= 0b00000001;
+    }
+    void reset_carry() {
+        FLAG &= 0b11111110;
+    }
+};
+
+enum {
+    B = 0,
+    C,
+    D,
+    E,
+    H,
+    L
+};
+
+enum {
+    A = 0,
+    INSTRUCTION,
+    ACC
+};
+
+#endif /* flag_h */
