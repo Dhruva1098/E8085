a5e1cfc7074ed2a37eecca1d55bc5b419ef48726
Dhruva Sharma <dhruvasharma@Dhruvas-MBP.lan>
2024-06-21 11:56:27 +0530

    testing new arch

--- instruction_set.h
+++ instruction_set.h
@@ -7,11 +7,15 @@ extern uint16_t program_counter;
 extern uint16_t stack_pointer;
 
 enum {
-    "MOV" = 0,
-    "MVI",
-    "LDA",
-    "LDAX",
-    "STA"
+    MOV = 0,
+    MVI,
+    LDA,
+    LDAX,
+    STA,
+    XCHG,
+    SPHL,
+    PCHL,
+    PUSH
 }
 
 void MOV(uint8_t R, uint16_t M);
@@ -20,6 +24,12 @@ void MVI(uint8_t R, uint8_t data);
 void LDA(uint16_t M);
 void LDAX(uint8_t R);
 void STA(uint16_t M);
+void XCHG(uint8_t R, uint8_t R);
+void SPHL(); //HL -> SP
+void PCHL(); //PC -> HL
+void PUSH(uint8_t R);
+
+
 
 
 
