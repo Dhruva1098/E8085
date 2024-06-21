4ea6d9cc400e9cc2178d5e60361b813b67ece24f
Dhruva Sharma <dhruva1098@gmail.com>
2024-06-21 15:47:01 +0530

    testing

--- E8085/instruction_set.hpp
+++ E8085/instruction_set.hpp
@@ -6,16 +6,11 @@ extern uint8_t SPR[3]; //special purpose registers
 extern uint16_t program_counter;
 extern uint16_t stack_pointer;
 
+
 void MOV(uint8_t R, uint16_t M);
 void MOV(uint16_t M, uint8_t data);
 void MVI(uint8_t R, uint8_t data);
 void LDA(uint16_t M);
-void LDAX(uint8_t R);
-void STA(uint16_t M);
-void XCHG(uint8_t R1, uint8_t R2);
-void SPHL(); //HL -> SP
-void PCHL(); //PC -> HL
-void PUSH(uint8_t R);
 
 
 
