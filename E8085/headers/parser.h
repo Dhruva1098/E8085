6b9970dfee767ae3bdd0c2c88470b88469858f91
Dhruva Sharma <dhruva1098@gmail.com>
2024-06-27 17:07:19 +0530

    created parser header

---
+++ E8085/headers/parser.h
@@ -0,0 +1,7 @@
+// Copyright 2024 Dhruva Sharma
+
+#ifndef E8085_HEADERS_PARSER_H_
+#define E8085_HEADERS_PARSER_H_
+
+
+#endif  // E8085_HEADERS_PARSER_H_
--- E8085/parser.cpp
+++ E8085/parser.cpp
@@ -1,4 +1,7 @@
 // Copyright 2024 Dhruva Sharma
 
+#include "headers/parser.h"
+
 #include <iostream>
 #include <vector>
+
