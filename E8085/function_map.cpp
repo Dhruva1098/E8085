72d7900ab0fb734cd398eb7e9c4d8ebbd279eb2b
Dhruva Sharma <dhruva1098@gmail.com>
2024-07-02 20:07:33 +0530

    created instruction_map files

---
+++ E8085/function_map.cpp
@@ -0,0 +1,7 @@
+//
+//  function_map.cpp
+//  E8085
+//
+//  Created by Dhruva Sharma on 02/07/24.
+//
+
---
+++ E8085/headers/function_map.h
@@ -0,0 +1,7 @@
+//
+//  function_map.h
+//  E8085
+//
+//  Created by Dhruva Sharma on 02/07/24.
+//
+
