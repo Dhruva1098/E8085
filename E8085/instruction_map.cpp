370d75d968eafea2786a0af932804636a7c42a3f
Dhruva Sharma <dhruva1098@gmail.com>
2024-07-02 20:53:11 +0530

    updated instruction map
    added all onw word instructions

--- E8085/instruction_map.cpp
+++ E8085/instruction_map.cpp
@@ -8,6 +8,7 @@
 const std::map<std::string, std::function<void(const std::vector<std::string>&)>>& create_instruction_map() {
     static std::map<std::string, std::function<void(const std::vector<std::string>&)>> instruction_map;
     // Functions with 0 arguments
+    // SPHL
     instruction_map["SPHL"] = [](const std::vector<std::string>& args) {
         if (args.empty()) {
             SPHL();
@@ -15,5 +16,30 @@ const std::map<std::string, std::function<void(const std::vector<std::string>&)>
             std::cerr << "ERROR: function expects no arguments" << std::endl;
         }
     };
+    // PCHL
+    instruction_map["PCHL"] = [](const std::vector<std::string>& args) {
+        if (args.empty()) {
+            PCHL();
+        } else {
+            std::cerr << "ERROR: function expects no arguments" << std::endl;
+        }
+    };
+    // CMC
+    instruction_map["CMC"] = [](const std::vector<std::string>& args) {
+        if (args.empty()) {
+            CMC();
+        } else {
+            std::cerr << "ERROR: function expects no arguments" << std::endl;
+        }
+    };
+    // STC
+    instruction_map["STC"] = [](const std::vector<std::string>& args) {
+        if (args.empty()) {
+            STC();
+        } else {
+            std::cerr << "ERROR: function expects no arguments" << std::endl;
+        }
+    };
+
     return instruction_map;
 }
